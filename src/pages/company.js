d2378e8e5c8f9edfdbf564baea1ad7c8a4c0b70a
Ryo <58804120+udemy1013@users.noreply.github.com>
2025-01-17 12:34:47 +0900

    事業内容の変更

--- src/pages/company.js
+++ src/pages/company.js
@@ -1,60 +1,67 @@
-import React, {useState, useEffect}from "react";
+import React, { useState, useEffect } from "react";
 import { Grid, Typography, Button } from "@mui/material";
 import mark from "../components/images/color_mark.png";
 import BackButton from "../components/BackButton";
 import useLocationChange from "../components/useLocationChange.ts";
 
 const Company = () => {
-
-  const [iframeHeight, setIframeHeight] = useState('450');
+  const [iframeHeight, setIframeHeight] = useState("450");
 
   useEffect(() => {
     // Function to update iframe height based on window size
     const updateHeight = () => {
-        if(window.innerWidth <= 600) {  // Check if window width is less than or equal to 600px
-            setIframeHeight('300');
-        } else {
-            setIframeHeight('450');
-        }
+      if (window.innerWidth <= 600) {
+        // Check if window width is less than or equal to 600px
+        setIframeHeight("300");
+      } else {
+        setIframeHeight("450");
+      }
     };
 
     // Call function initially to set height based on initial window size
     updateHeight();
 
     // Set up event listener to update height whenever window is resized
-    window.addEventListener('resize', updateHeight);
+    window.addEventListener("resize", updateHeight);
 
     // Clean up event listener when component unmounts
-    return () => window.removeEventListener('resize', updateHeight);
-}, []);
+    return () => window.removeEventListener("resize", updateHeight);
+  }, []);
 
-    const leftInfo = [
-        '会社名',
-        '設立',
-        '所在地',
-        '代表取締役',
-        '事業内容',
-        '従業員数',
-      ];
+  const leftInfo = [
+    "会社名",
+    "設立",
+    "所在地",
+    "代表者",
+    "事業内容",
+    "従業員数",
+  ];
 
-    const rightInfo = [
-        '合同会社アリガトサン',
-        '2024年5月',
-        '〒660-0892 兵庫県尼崎市東難波町5丁目8-26リアライズ尼崎2A',
-        '吉川遼',
-        'HP制作、ソフトウェア開発、IP事業、アパレル事業',
-        '9名(業務委託1名)',
-      ];
-      
+  const rightInfo = [
+    "合同会社アリガトサン",
+    "2024年5月",
+    "〒660-0892 兵庫県尼崎市東難波町5丁目8-26リアライズ尼崎2A",
+    "吉川遼",
+    "ソフトウェア開発、AIアプリケーション開発、HP制作、Wordpress修繕、IP事業、アパレル事業",
+    "9名(業務委託1名)",
+  ];
+
   return (
     <div>
-    <Grid
+      <Grid
         className="title"
         container
         spacing={5}
-        sx={{ maxWidth: "1536px", margin: "auto", mt: 5, padding: "0 24px" ,  justifyContent: "end",  width: "100%" }}
+        sx={{
+          maxWidth: "1536px",
+          margin: "auto",
+          mt: 5,
+          padding: "0 24px",
+          justifyContent: "end",
+          width: "100%",
+        }}
       >
-         <Typography
+        <Typography
           variant="h1"
           sx={{
             fontSize: "64px",
@@ -62,23 +69,37 @@ const Company = () => {
             color: "#FFFFFF",
             fontFamily: "Lexend",
             mb: 1,
-            marginRight: {xs:0, md:"40px"}
+            marginRight: { xs: 0, md: "40px" },
           }}
           className="title"
         >
           company
         </Typography>
       </Grid>
-         
+
       <Grid
         container
         spacing={5}
-        sx={{ maxWidth: "1536px", margin: "auto", padding: "0 24px", justifyContent: "space-between"  ,width: "100%" }}
+        sx={{
+          maxWidth: "1536px",
+          margin: "auto",
+          padding: "0 24px",
+          justifyContent: "space-between",
+          width: "100%",
+        }}
       >
-        
-        <Grid item lg={4} xs={12} sx={{ textAlign: "left", pt:0,  "@media screen and (max-width:1200px)": {
-       paddingRight: "40px"
-    }}}>
+        <Grid
+          item
+          lg={4}
+          xs={12}
+          sx={{
+            textAlign: "left",
+            pt: 0,
+            "@media screen and (max-width:1200px)": {
+              paddingRight: "40px",
+            },
+          }}
+        >
           <div className="relative">
             <iframe
               iframe
@@ -95,26 +116,30 @@ const Company = () => {
             <img id="company-mark" className="just-absolute" src={mark} />
           </div>
         </Grid>
-        <Grid item lg={7} xs={12} sx={{ textAlign: "left", pl: 5 , pt:0, marginRight: "40px"}}>
-
+        <Grid
+          item
+          lg={7}
+          xs={12}
+          sx={{ textAlign: "left", pl: 5, pt: 0, marginRight: "40px" }}
+        >
           <div>
-          <hr className="line" />
-          {leftInfo.map((item, index) => (
-                <React.Fragment key={index}>
+            <hr className="line" />
+            {leftInfo.map((item, index) => (
+              <React.Fragment key={index}>
                 <div className="container">
                   <p className="nospace">{item}</p>
                   <p>{rightInfo[index]}</p>
                 </div>
                 {index !== leftInfo.length - 1 && <hr className="line" />}
-                </React.Fragment>
-              ))}
-              <hr className="line" />
+              </React.Fragment>
+            ))}
+            <hr className="line" />
           </div>
         </Grid>
       </Grid>
-      <Grid container sx={{ justifyContent: "center", mb:5 }} spacing={3} >
-        <Grid item sx={{mt:5 ,mb: 5}}>
-         <BackButton/>
+      <Grid container sx={{ justifyContent: "center", mb: 5 }} spacing={3}>
+        <Grid item sx={{ mt: 5, mb: 5 }}>
+          <BackButton />
         </Grid>
       </Grid>
     </div>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,37 +1,46 @@
 import React from "react";
-import { Grid, Typography, Button, Box, Link, useTheme, useMediaQuery, styled } from "@mui/material";
+import {
+  Grid,
+  Typography,
+  Button,
+  Box,
+  Link,
+  useTheme,
+  useMediaQuery,
+  styled,
+} from "@mui/material";
 import phone from "../components/images/phone.png";
 import BackButton from "../components/BackButton";
-import useLocationChange from '../components/useLocationChange.ts';
+import useLocationChange from "../components/useLocationChange.ts";
 
 // カスタムスタイルコンポーネントの作成
 const CustomButton = styled(Button)(({ theme }) => ({
-  '&.MuiButtonBase-root': {
+  "&.MuiButtonBase-root": {
     textTransform: "uppercase !important",
-    minWidth: '150px',
+    minWidth: "150px",
     backgroundColor: "transparent",
     color: "#FFFFFF !important",
     border: "1px solid #FFFFFF !important",
-    borderRadius: "30px !important",  // グローバルスタイルに合わせる
-    padding: "5px 35px !important",   // グローバルスタイルに合わせる
+    borderRadius: "30px !important", // グローバルスタイルに合わせる
+    padding: "5px 35px !important", // グローバルスタイルに合わせる
     fontSize: "14px",
     letterSpacing: "0.5px",
     fontWeight: "400",
     boxShadow: "none",
     transition: "all 0.3s ease",
   },
-  '&.MuiButtonBase-root:hover': {
+  "&.MuiButtonBase-root:hover": {
     backgroundColor: "#FFFFFF !important",
     color: "#000000 !important",
     border: "1px solid #FFFFFF !important",
-  }
+  },
 }));
 
 function Contact() {
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
-  const emailAddress = "t.ryosuke@arigatosun.com";
+  const emailAddress = "k2@arigatosun.com";
   const contactText = `依頼に関しまして、メールにてご相談を承っております。
                       下記のメールアドレスまで、ご相談内容をお送りください。
                       1~2営業日で担当者からご返信させていただきます。`;
@@ -41,7 +50,13 @@ function Contact() {
       <Grid
         container
         spacing={2}
-        sx={{ maxWidth: "1536px", margin: "auto", alignItems: "flex-end", padding: "0px 24px", width: "100%" }}
+        sx={{
+          maxWidth: "1536px",
+          margin: "auto",
+          alignItems: "flex-end",
+          padding: "0px 24px",
+          width: "100%",
+        }}
       >
         <Grid item md={6} xs={12} className="pr-16-md center-sm">
           <Typography
@@ -56,44 +71,79 @@ function Contact() {
           >
             contact
           </Typography>
-          <p style={{ fontSize: "16px", lineHeight: "31px" }} className="display-md">
+          <p
+            style={{ fontSize: "16px", lineHeight: "31px" }}
+            className="display-md"
+          >
             {contactText}
             <br />
             <span style={{ fontWeight: "500" }}>Email: {emailAddress}</span>
           </p>
         </Grid>
-        <Grid item md={6} xs={12} sx={{ textAlign: "right" }} className="center-sm">
-          <img 
-            id="phone" 
-            src={phone} 
-            width={280} 
-            alt="phone" 
-            style={{ 
+        <Grid
+          item
+          md={6}
+          xs={12}
+          sx={{ textAlign: "right" }}
+          className="center-sm"
+        >
+          <img
+            id="phone"
+            src={phone}
+            width={280}
+            alt="phone"
+            style={{
               maxWidth: "100%",
-              height: "auto"
+              height: "auto",
             }}
           />
         </Grid>
       </Grid>
       <div className="display-sm">
-        <p style={{ fontSize: "16px", lineHeight: "31px", textAlign: "left" }} className="descrption-mobile">
+        <p
+          style={{ fontSize: "16px", lineHeight: "31px", textAlign: "left" }}
+          className="descrption-mobile"
+        >
           {contactText}
           <br />
           <span style={{ fontWeight: "500" }}>Email: {emailAddress}</span>
         </p>
       </div>
-      
-      <Grid container sx={{mt:3, mb: 5, flexDirection:{md:"row",sm:"column-reverse", xs:"column-reverse"}}} spacing={3}>
+
+      <Grid
+        container
+        sx={{
+          mt: 3,
+          mb: 5,
+          flexDirection: {
+            md: "row",
+            sm: "column-reverse",
+            xs: "column-reverse",
+          },
+        }}
+        spacing={3}
+      >
         <Grid item md={6} xs={12}>
-          <Box sx={{ textTransform: "uppercase !important", textAlign: {md:"end", xs:"center"} }}>
+          <Box
+            sx={{
+              textTransform: "uppercase !important",
+              textAlign: { md: "end", xs: "center" },
+            }}
+          >
             <BackButton />
           </Box>
         </Grid>
-        <Grid item sx={{ textAlign: {md:"start", xs:"center"}}} md={6} xs={12}>
-          <Link href={`mailto:${emailAddress}`} style={{ textDecoration: 'none' }}>
-            <CustomButton variant="outlined">
-              Send Email
-            </CustomButton>
+        <Grid
+          item
+          sx={{ textAlign: { md: "start", xs: "center" } }}
+          md={6}
+          xs={12}
+        >
+          <Link
+            href={`mailto:${emailAddress}`}
+            style={{ textDecoration: "none" }}
+          >
+            <CustomButton variant="outlined">Send Email</CustomButton>
           </Link>
         </Grid>
       </Grid>
